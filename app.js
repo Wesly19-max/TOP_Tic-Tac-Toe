b6264ef1bdb1b38be9a69b0dd710a8430e939906
Wesly Pardillo <200398940+Wesly19-max@users.noreply.github.com>
2026-05-15 14:46:04 +0800

    Fix bug and create gameController module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const GameBoard = () => {
+function GameBoard() {
   const rows=3;
   const columns=3;
   const board=[];
@@ -18,10 +18,12 @@ const GameBoard = () => {
   }
 
   const printBoard =() => {
-    const boardWithCellValues = board.map((row) => {
-      row.map((cell) => cell.getValue());
-    })
-  }
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+  );
+
+    console.log(boardWithCellValues);
+  };
 
   return {getBoard,writeMark,printBoard};
 }
@@ -30,10 +32,68 @@ function Cell() {
   let value = 0;
 
   const addMark = (player) => {
-    value = player
+    value = player;
   }
 
   const getValue = () => value;
 
   return {addMark,getValue};
 }
+
+const GameController = ((
+  playerOneName = "Player One",
+  playerTwoName = "Player Two"
+) => {
+
+  const board = GameBoard();
+
+  const Players = [
+    {
+      name:playerOneName,
+      token:"X"
+    },
+    {
+      name:playerTwoName,
+      token:"O"
+    }
+  ];
+
+  let activePlayer = Players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === Players[0] ? Players[1] :Players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn `);
+  };
+
+  const playRound =(row,col) => {
+    console.log(
+      `Writing ${getActivePlayer().name}'s mark into row ${row}, column ${col}...`
+    );
+
+    board.writeMark(row,col,getActivePlayer().token);
+
+    //handles win condition logic and message
+
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  //initial game message
+  printNewRound();
+
+  
+  return {
+    playRound,
+    getActivePlayer,
+  };
+
+
+
+})();
+
