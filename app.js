f8a3789e3d9e58ca71d22ae5439bff5f7c1cdd5d
Wesly Pardillo <200398940+Wesly19-max@users.noreply.github.com>
2026-05-15 14:01:54 +0800

    Create GameBoard factory function

---
+++ app.js
@@ -0,0 +1,29 @@
+const GameBoard = () => {
+  const rows=3;
+  const columns=3;
+  const board=[];
+
+  //create gameBoard
+  for (let i=0;i<rows;i++) {
+    board[i] = [];
+    for (let j=0;j<columns;j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const writeMark = (row,col,player) => {
+    board[row][col].addMark(player);
+  }
+
+  const printBoard =() => {
+    const boardWithCellValues = board.map((row) => {
+      row.map((cell) => cell.getValue());
+    })
+  }
+
+  return {getBoard,writeMark,printBoard};
+}
+
+
